181e9862fa7aeeffb28685cb1fb25945922861fd
Oleh Nychyporenko <nich.oleg@ukr.net>
2025-03-16 13:02:27 +0200

    implement py-herbivores-and-carnivores

--- app/main.py
+++ app/main.py
@@ -1,34 +1,16 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, health: int = 100) -> None:
-        self.name = name
+    def __init__(self, name: str, health: int = 100,
+                 hidden: bool = False) -> None:
         self.health = health
-        self.hidden = False
+        self.name = name
+        self.hidden = hidden
         Animal.alive.append(self)
 
-    def take_damage(self, damage: int) -> None:
-        self.health -= damage
-        if self.health < 0:
-            self.health = 0
-        print(f"{self.name} took {damage} damage. Health: {self.health}")
-
-        if self.health == 0:
-            self.die()
-
-    def die(self) -> None:
-        if self in Animal.alive:
-            Animal.alive.remove(self)
-        print(f"{self.name} is dead.")
-
-    def is_alive(self) -> bool:
-        return self.health > 0
-
     def __repr__(self) -> str:
-        return (f"{{Name: {self.name}, "
-                f"Health: {self.health}, "
-                f"Hidden: {self.hidden}}}"
-                )
+        return (f"{{Name: {self.name}, Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
 
 
 class Herbivore(Animal):
@@ -37,15 +19,9 @@ class Herbivore(Animal):
 
 
 class Carnivore(Animal):
-    def bite(self, herbivore: Herbivore) -> None:
-        if isinstance(herbivore, Herbivore):
-            if herbivore.hidden:
-                print(f"{self.name} cannot bite {herbivore.name} "
-                      f"because they are hidden.")
-            else:
-                herbivore.take_damage(50)
-                print(f"{self.name} bit {herbivore.name}. "
-                      f"{herbivore.name} s health: {herbivore.health}"
-                      )
-        else:
-            print(f"{self.name} cannot bite another carnivore.")
+    def bite(self, animal: Animal) -> None:
+        if (not animal.hidden and animal in animal.alive
+                and isinstance(animal, Herbivore)):
+            animal.health -= 50
+            if animal.health <= 0:
+                Animal.alive.remove(animal)
